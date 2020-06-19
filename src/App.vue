811945fbf3364d7592ca51239afcdecc75ed5577
Pedro Alexandre Romano Lopes <pedroalexlopes25@gmail.com>
2020-06-19 17:34:14 +0100

    Use mapState, mapGetters

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    Root Foo: {{rootFoo}} <br/>
+    Robots Foo: {{robotsFoo}} <br/>
+    Users Foo: {{usersFoo}} <br/>
+    <br/>
+    Root Getter Foo: {{rootGetterFoo}} <br/>
+    Robots Getter Foo: {{robotsGetterFoo}} <br/>
+
     <header>
       <nav>
         <ul>
@@ -37,9 +44,18 @@
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex';
+
 export default {
-  name: 'App',
+  name: 'app',
   computed: {
+    ...mapState({
+      rootFoo: (state) => state.foo,
+      usersFoo: (state) => { console.log(state); return 'poop'; }, // state.users.foo }, // this no works
+    }),
+    ...mapState('robots', { robotsFoo: 'foo' }),
+    ...mapGetters({ rootGetterFoo: 'foo' }),
+    ...mapGetters('robots', { robotsGetterFoo: 'foo' }),
     cart() {
       return this.$store.state.robots.cart;
     },
