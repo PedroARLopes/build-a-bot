3b2996aeb72b2f49887609ca2ac73ea5bd0c595e
Pedro Alexandre Romano Lopes <pedroalexlopes25@gmail.com>
2020-06-20 11:55:14 +0100

    Refactor Sale span to use directives

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -11,7 +11,12 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span @click="pinPadding='30px'"
+          v-pin="{bottom:pinPadding, right:pinPadding}"
+          class="sale"
+          v-show="selectedPart.onSale">
+      Sale!
+    </span>
   </div>
 </template>
 
@@ -41,7 +46,10 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return {
+      selectedPartIndex: 0,
+      pinPadding: '10px',
+    };
   },
   computed: {
     selectedPart() {
@@ -83,9 +91,6 @@ export default {
 }
 
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
